77bf4e2aa3ebf293257affea668eb4e4ec498819
AtomJay <ifatomjay@gmail.com>
2022-06-17 09:09:14 +0800

    add postgresql connection

--- DataEngineering/Chapter5/5.4/financialdata/clients.py
+++ DataEngineering/Chapter5/5.4/financialdata/clients.py
@@ -17,3 +17,17 @@ def get_mysql_financialdata_conn() -> engine.base.Connection:
     engine = create_engine(address)
     connect = engine.connect()
     return connect
+
+def get_postgre_financialdata_conn() -> engine.base.Connection:
+    """
+    user: postgres
+    password: 123456
+    host: localhost
+    port: 5432
+    database: financialdata
+    如果有實體 IP，以上設定可以自行更改
+    """
+    address = "postgresql://postgres:123456@localhost:5432/FinancialData"
+    engine = create_engine(address)
+    connect = engine.connect()
+    return connect
